99ef470ef28d25b449de18ee71b6c1a094144a49
Andrew <andrew@establishsolutions.com>
2017-08-01 11:53:28 -0400

    added widgets in footer.php

--- footer.php
+++ footer.php
@@ -14,6 +14,55 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		
+		<div class="footer-widgets">
+
+			<?php if (is_active_sidebar('footer1')) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar('footer1'); ?>
+			</div>
+			<!--footer-widget-area-->
+			<?php endif; ?>
+
+			<?php if (is_active_sidebar('footer2')) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar('footer2'); ?>
+			</div>
+			<!--footer-widget-area-->
+			<?php endif; ?>
+
+			<?php if (is_active_sidebar('footer3')) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar('footer3'); ?>
+			</div>
+			<!--footer-widget-area-->
+			<?php endif; ?>
+
+			<?php if (is_active_sidebar('footer4')) : ?>
+			<div class="footer-widget-area">
+				<?php dynamic_sidebar('footer4'); ?>
+			</div>
+			<!--footer-widget-area-->
+			<?php endif; ?>
+
+		</div>
+		<!--footer-widgets-->
+
+		<nav class="site-nav">
+
+			<?php
+
+				$args = array(
+					'theme_location' => 'footer'
+				);
+
+			?>
+
+				<?php wp_nav_menu(  $args ); ?>
+
+		</nav>
+
+		
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://peakdigitalagency.com', 'underscores' ) ); ?>"><?php
 				printf( esc_html__( 'This theme was developed by %s', 'underscores' ), 'Peak Digital Agency' );
